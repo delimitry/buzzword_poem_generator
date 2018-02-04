1b967de914187c6d2bbaa8420ecfd2baa6a9da87
Dmitry Alimov <dvalimov@gmail.com>
2018-02-04 12:20:39 +0300

    Update buzzword_poem_generator.py

--- buzzword_poem_generator.py
+++ buzzword_poem_generator.py
@@ -12,6 +12,7 @@ import itertools
 import os
 import pickle
 import random
+from collections import Counter
 
 
 MAX_TRIES = 100
@@ -106,27 +107,28 @@ def invert_map(map_val):
     return inv_map
 
 
-# build map of {syllables: buzzwords with syllables}
-SYLLABLES_WORDS = invert_map(WORDS_SYLLABLES)
-
-SYLLABLES_KEYS = sorted([0] + list(SYLLABLES_WORDS.keys()))
-KEYS_HASH = hash(tuple(SYLLABLES_KEYS)) & 0xffffffff
-
-
-def find_poem_base(combinations, syllables_in_lines, min_words_in_line=3):
+def find_poem_base(syllables_words, syl_combinations, syllables_in_lines, min_words_in_line=3):
     """Find poem base with required number of syllables in lines"""
+    if min_words_in_line <= 0:
+        raise Exception('Min words in line must be greater than or equal to 1!')
     if min_words_in_line > max(syllables_in_lines):
         raise Exception('Min words in line is more than max number of syllables in lines!')
-
-    for syllables in combinations:
-        random.shuffle(combinations[syllables])
+    # if some syllable key was not found in {syllables: combinations} dict
+    for syllables_in_line in syllables_in_lines:
+        if syllables_in_line not in syl_combinations:
+            raise Exception('Combinations for syllables {} were not found!'.format(syllables_in_line))
+    for syllables in syl_combinations:
+        random.shuffle(syl_combinations[syllables])
     # total available words for syllable {syllables: words count}
-    total_available = {x: len(SYLLABLES_WORDS[x]) for x in SYLLABLES_WORDS}
+    total_available = {x: len(syllables_words[x]) for x in syllables_words}
     found_lines = []
 
     # find a combination for each required number of syllables in line
     for syllables_in_line in syllables_in_lines:
-        for combination in combinations[syllables_in_line]:
+        # stop if found all the lines
+        if len(found_lines) >= len(syllables_in_lines):
+            break
+        for combination in syl_combinations[syllables_in_line]:
             if len(combination) < min_words_in_line:
                 continue
             # make map {syllables: words count}, i.e. [2, 3, 2] -> {2: 2, 3: 1}
@@ -135,7 +137,7 @@ def find_poem_base(combinations, syllables_in_lines, min_words_in_line=3):
             suitable = True
             for syllables, words_count in cur_used.items():
                 # find next combination if words count in current one is more than left available
-                if words_count > total_available[syllables]:
+                if words_count > total_available.get(syllables, 0):
                     suitable = False
             if not suitable:
                 continue
@@ -143,19 +145,17 @@ def find_poem_base(combinations, syllables_in_lines, min_words_in_line=3):
             for syllables, words_count in cur_used.items():
                 total_available[syllables] -= words_count
             found_lines.append(combination)
-            break
-        # stop if found all the lines
-        if len(found_lines) >= len(syllables_in_lines):
-            break
+            if len(found_lines) >= len(syllables_in_lines):
+                break
     # return empty lines if not found all required lines
     if len(found_lines) < len(syllables_in_lines):
         return []
     return found_lines
 
 
-def get_rhyme_words_from_syllables_num(syllables_num, used_rhyme_words=None):
+def get_rhyme_words_from_syllables_num(words_syllables, syllables_words, rhymes, syllables_num, used_rhyme_words=None):
     """
-    Get rhyme words from the list of syllables count
+    Get rhyme words for the list of syllables count `syllables_num`
     E.g. for syllables_num [1, 2] returns ['Sqoop', 'Hadoop']
     Use argument used_rhyme_words to set already used words
     """
@@ -167,10 +167,10 @@ def get_rhyme_words_from_syllables_num(syllables_num, used_rhyme_words=None):
     # if length of syllables_num is 1, return random word that has no rhyme
     if len(syllables_num) == 1:
         if used_rhyme_words:
-            available_words = copy.deepcopy(WORDS_SYLLABLES)
+            available_words = copy.deepcopy(words_syllables)
             # remove all rhymes for used rhyme words from available words
             for used_rhyme_word in used_rhyme_words:
-                for rhyme_line in RHYMES:
+                for rhyme_line in rhymes:
                     if used_rhyme_word in rhyme_line:
                         for rhyme_word in rhyme_line:
                             if rhyme_word in available_words:
@@ -181,14 +181,15 @@ def get_rhyme_words_from_syllables_num(syllables_num, used_rhyme_words=None):
             available_syllables_words = invert_map(available_words)
         else:
             # if used_rhyme_words is empty just get the random word with syllables_num
-            available_syllables_words = SYLLABLES_WORDS
+            available_syllables_words = syllables_words
+        if syllables_num[0] not in available_syllables_words:
+            return []
         return [random.choice(available_syllables_words[syllables_num[0]])]
 
-    syllables_num_count = {}
-    for n in syllables_num:
-        syllables_num_count[n] = syllables_num_count.get(n, 0) + 1
+    # build a map {number of syllables: count} from syllables_num
+    syllables_num_count = Counter(syllables_num)
     # remove already used rhyme words from rhymes
-    unique_rhymes = [list(r) for r in RHYMES]
+    unique_rhymes = [list(r) for r in rhymes]
     for rhyme_line in unique_rhymes:
         for used_rhyme_word in used_rhyme_words:
             if used_rhyme_word in rhyme_line:
@@ -196,27 +197,33 @@ def get_rhyme_words_from_syllables_num(syllables_num, used_rhyme_words=None):
     # find the rhymes for the list of syllables count
     found_rhymes = []
     for rhyme_line in unique_rhymes:
-        rhyme_syllables = [WORDS_SYLLABLES[r] for r in rhyme_line]
-        # build a map of {number of syllables: list of rhymes}
+        rhyme_syllables = [words_syllables[r] for r in rhyme_line]
+        # build a map {number of syllables: list of rhymes}
         nd = {}
-        for num in syllables_num:
-            for rs_num, rh in zip(rhyme_syllables, rhyme_line):
-                if num == rs_num:
+        for rs_num, rh in zip(rhyme_syllables, rhyme_line):
+            for num in syllables_num:
+                if rs_num == num:
                     nd[num] = list(set(nd.get(num, []) + [rh]))
-        # if found all syllables_num - add to found list
+        # build a map {number of syllables: list of rhymes length}
         syllables_count = {k: len(v) for k, v in nd.items()}
-        if syllables_count == syllables_num_count:
+        # if the length of found rhymes >= required syllables number count - add to found list
+        add = True
+        for num in syllables_num_count:
+            if syllables_count.get(num, 0) < syllables_num_count[num]:
+                add = False
+        if add:
+            # add {number of syllables: list of rhymes} to found rhymes list
             found_rhymes.append(nd)
     if not found_rhymes:
         return []
-    # pick random rhyme from found list
+    # pick random {number of syllables: list of rhymes} from found rhymes list
     found_rhyme = random.choice(found_rhymes)
-    # pop first word  with `syllables_num` from `found_rhyme` list
-    result = [found_rhyme[n].pop(0) for n in syllables_num]
+    # pop random word  with `syllables_num` from `found_rhyme` list
+    result = [found_rhyme[n].pop(random.randint(0, len(found_rhyme[n]) - 1)) for n in syllables_num]
     return result
 
 
-def get_rhyme_words_groups(syllables_num_groups):
+def get_rhyme_words_groups(words_syllables, syllables_words, rhymes, syllables_num_groups):
     """
     Get the groups of rhyme words from the groups of the syllables count lists
     E.g. for groups = [1, 2], [1, 3] returns ['Sqoop', 'Hadoop'], ['Go', 'TensorFlow']
@@ -224,7 +231,11 @@ def get_rhyme_words_groups(syllables_num_groups):
     result = []
     used_rhyme_words = set()
     for group in syllables_num_groups:
-        rhyme_words = get_rhyme_words_from_syllables_num(group, used_rhyme_words=used_rhyme_words)
+        # skip empty groups
+        if not group:
+            continue
+        rhyme_words = get_rhyme_words_from_syllables_num(
+            words_syllables, syllables_words, rhymes, group, used_rhyme_words=used_rhyme_words)
         if not rhyme_words:
             return []
         used_rhyme_words |= set(rhyme_words)
@@ -232,29 +243,29 @@ def get_rhyme_words_groups(syllables_num_groups):
     return result
 
 
-def fill_poem(found_base_lines, rhyme_scheme):
+def fill_poem(words_syllables, syllables_words, rhymes, found_base_lines, rhyme_scheme):
     """Fill the poem with the words, as the poem base consists of the number of syllables"""
     if len(found_base_lines) != len(rhyme_scheme):
         raise Exception('The number of lines != rhyme scheme!')
 
     # group last words in found lines by rhyme scheme letters
     # for example from [('A', 1), ('B', 1), ('A', 1), ('B', 3)] to {'A': [1, 1], 'B': [1, 3]}
-    sheme_last_word = {}
+    scheme_last_word = {}
     for k, v in zip(rhyme_scheme, [x[-1] for x in found_base_lines]):
-        sheme_last_word[k] = sheme_last_word.get(k, []) + [v]
+        scheme_last_word[k] = scheme_last_word.get(k, []) + [v]
 
     # get the groups of rhyme words from the groups of syllables num
-    rhyme_words_groups = get_rhyme_words_groups(sheme_last_word.values())
+    rhyme_words_groups = get_rhyme_words_groups(words_syllables, syllables_words, rhymes, scheme_last_word.values())
     if not rhyme_words_groups:
         return []
 
-    rhyme_scheme_letter_last_words = dict(zip(sheme_last_word.keys(), rhyme_words_groups))
-    current_syllables_words = copy.deepcopy(SYLLABLES_WORDS)
+    rhyme_scheme_letter_last_words = dict(zip(scheme_last_word.keys(), rhyme_words_groups))
+    current_syllables_words = copy.deepcopy(syllables_words)
     # flatten values lists
     last_words_values = sum(rhyme_scheme_letter_last_words.values(), [])
     # remove last words
     for last_word in last_words_values:
-        current_syllables_words[WORDS_SYLLABLES[last_word]].remove(last_word)
+        current_syllables_words[words_syllables[last_word]].remove(last_word)
 
     # fill the poem with words
     poem = []
@@ -273,31 +284,68 @@ def is_rhyme(words, rhymes):
     return False
 
 
-def get_syllables_combinations(syllables_num, use_cache=True):
+def get_syllables_combinations(syllables_words, syllables_num, use_cache=True):
     """
     Get all possible compositions of `syllables_num`, i.e. all combinations,
     where a sum of positive integers is equal to `syllables_num`
     """
+    # calc syllables keys hash
+    syllables_keys = sorted([0] + list(syllables_words.keys()))
+    keys_hash = hash(tuple(syllables_keys)) & 0xffffffff
     combinations = []
     # if already computed and cache is used - load combinations from file
-    fn = 'compositions_{}_{}.dat'.format(KEYS_HASH, syllables_num)
+    fn = 'compositions_{}_{}.dat'.format(keys_hash, syllables_num)
     if os.path.isfile(fn) and use_cache:
         with open(fn, 'rb') as f:
             combinations = pickle.load(f)
     else:
-        for v in itertools.product(SYLLABLES_KEYS, repeat=syllables_num):
+        for v in itertools.product(syllables_keys, repeat=syllables_num):
             if sum(v) == syllables_num:
-                if v not in combinations:
-                    combinations.append([x for x in v if x])
+                fv = [x for x in v if x]
+                if fv not in combinations:
+                    combinations.append(fv)
         if use_cache:
-            with open('compositions_{}_{}.dat'.format(KEYS_HASH, syllables_num), 'wb') as f:
+            with open('compositions_{}_{}.dat'.format(keys_hash, syllables_num), 'wb') as f:
                 f.write(pickle.dumps(combinations))
     return combinations
 
 
+def generate_poem(rhyme_scheme, syllables_in_lines, min_words_in_line, use_cache):
+    """Generate poem"""
+    try:
+        # build map of {syllables: words with syllables}
+        syllables_words = invert_map(WORDS_SYLLABLES)
+
+        if len(rhyme_scheme) != len(syllables_in_lines):
+            raise Exception('The rhyme scheme size is not equal to number of syllables in lines!')
+        if sum(syllables_in_lines) > sum(WORDS_SYLLABLES.values()):
+            raise Exception('The sum of syllables in lines is more than sum of all syllables in available words!')
+
+        syl_combinations = {
+            syls: get_syllables_combinations(syllables_words, syls, use_cache) for syls in syllables_in_lines}
+
+        # try to find poem in MAX_TRIES tries
+        try_number = 0
+        while True:
+            if try_number > MAX_TRIES:
+                print("A poem can't be generated :(")
+                break
+            poem_base = find_poem_base(
+                syllables_words, syl_combinations, syllables_in_lines, min_words_in_line=min_words_in_line)
+            if poem_base:
+                poem = fill_poem(WORDS_SYLLABLES, syllables_words, RHYMES, poem_base, rhyme_scheme)
+                if poem:
+                    print('\n'.join([' '.join(line) for line in poem]))
+                    break
+            try_number += 1
+            continue
+    except Exception as ex:
+        print('Error: {}'.format(ex))
+
+
 def main():
     """Main"""
-    # prepare argument parser
+    # prepare argument parser, add required arguments group before optional arguments
     parser = argparse.ArgumentParser(description='Buzzword poem generator')
     optional = parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
@@ -325,30 +373,7 @@ def main():
     min_words_in_line = args.min_words_in_line
     use_cache = args.cache.lower() in ['1', 'true', 't', 'on']
 
-    try:
-        if len(rhyme_scheme) != len(syllables_in_lines):
-            raise Exception('The rhyme scheme size is not equal to number of syllables in lines!')
-        if sum(syllables_in_lines) > sum(WORDS_SYLLABLES.values()):
-            raise Exception('The sum of syllables in lines is more than sum of all syllables in available words!')
-
-        combinations = {syls: get_syllables_combinations(syls, use_cache) for syls in syllables_in_lines}
-
-        # try to find poem in MAX_TRIES tries
-        try_number = 0
-        while True:
-            if try_number > MAX_TRIES:
-                print("A poem can't be generated :(")
-                break
-            poem_base = find_poem_base(combinations, syllables_in_lines, min_words_in_line=min_words_in_line)
-            if poem_base:
-                poem = fill_poem(poem_base, rhyme_scheme)
-                if poem:
-                    print('\n'.join([' '.join(line) for line in poem]))
-                    break
-            try_number += 1
-            continue
-    except Exception as ex:
-        print('Error: {}'.format(ex))
+    generate_poem(rhyme_scheme, syllables_in_lines, min_words_in_line, use_cache)
 
 
 if __name__ == '__main__':
